f818028028fd40bb623de21981a58e31964b31aa
john git <john@prpl.wtf>
2026-06-16 10:48:02 +0200

    add file upload

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>hello world</div>
+    <div class="flex flex-col justify-center h-full fixed w-full">
+        <div
+            class="flex flex-row items-center align-middle justify-center w-full"
+        >
+            <UFileUpload
+                label="Drop your file here"
+                description="1GB max"
+                class="w-96 min-h-48"
+            />
+        </div>
+    </div>
 </template>
