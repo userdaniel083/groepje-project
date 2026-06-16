b2039d5dd62ed71f8cf000f7d6c4389c7a567e53
userdaniel083 <6050351@mborijnland.nl>
2026-06-16 11:46:07 +0200

    Create server.py

---
+++ backend/server.py
@@ -0,0 +1,11 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at port {PORT}")
+    # Start de server en het stop niet automatisch, zodat het blijft draaien totdat je het handmatig stopt
+    httpd.serve_forever()
